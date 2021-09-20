db0ecff7ee40fb00c98250aae217c91066649f3d
kjam0 <south.kay@gmail.com>
2021-09-20 18:24:52 +1000

    Added validation rules so that score format requires nn-nn format and allows for overtime

--- app/models/match.rb
+++ app/models/match.rb
@@ -13,4 +13,41 @@ class Match < ApplicationRecord
   has_many :players, through: :match_players
 
   validates :time, presence: true
+  validate :valid_score, unless: :score_blank?
+
+  private
+
+  def score_blank?
+    self.score.blank?
+  end
+
+  def valid_score
+    # format the string into integers
+    score = self.score.split("-")
+
+    # Throw an error if user enters letters instead of integers
+    begin
+      score[0] = Integer(score[0])
+      score[1] = Integer(score[1])
+    rescue
+      errors.add(:score, "must contain '-' and numbers")
+      return
+    end
+
+    score.sort!.reverse!
+    if score[0] < 21
+      errors.add(:score, "must have a winner with at least 21")
+      return
+    end
+
+    if score[0] >= 21 && (score[0] - score[1]).abs < 2
+      errors.add(:score, "must be within 2 points of each other")
+      return
+    end
+
+    if score[0] > 21 && (score[0] - score[1]).abs > 2
+      errors.add(:score, "must not be greater than 21")
+      return
+    end
+  end
 end
